55d43ff599dee108b89da8f1a516f1e1d2db015b
Josh Lee <Josh@qccreative.com>
2014-03-31 13:19:45 -0400

    Initial commit - bwp-minify tweaks plugin

---
+++ bwp-minify-fix.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Plugin Name: BWP Minify Fix
+Plugin URI: http://qccreative.com/
+Description: Fixes BWP minify conflicts with admin bar styles.
+Version: 0.1.0
+Author: Josh Lee / QC Creative
+Author URI: http://qccreative.com
+License: GPLv3
+*/
+function mu_tweaks() {
+	//Exclude admin bar from bwp minify
+	function exclude_my_css($excluded)
+	{
+	    //admin-bar and dashicons are from wp
+	    //boxes is for WordPress SEO by Yoast
+	    //which adds a "score" icon to the admin
+	    //bar.
+	    $excluded = array('admin-bar','dashicons','boxes');
+	    return $excluded;
+	}
+	add_filter('bwp_minify_style_ignore', 'exclude_my_css');
+}
+add_action( 'init', 'mu_tweaks' );
